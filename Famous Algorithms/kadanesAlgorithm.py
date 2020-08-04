57428731e83d839c432b587e58bd20a3b758c345
Sagnik Ghosh <58620359+sagnikghoshcr7@users.noreply.github.com>
2020-08-05 00:55:56 +0530

    Add files via upload

---
+++ Famous Algorithms/kadanesAlgorithm.py
@@ -0,0 +1,20 @@
+"""
+Kadane's Algorithm
+Write a function that takes in a non-empty array of integers and returns the maximum sum that can be obtained
+by summing up all of the integers in a non-empty subarray of the input array.
+A subarray must only contain adjacent numbers.
+
+Sample Input: [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+Sample Output: 19 ([1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1])
+"""
+
+
+# O(n) time | O(1) space - where n is the length of the input array
+def kadanesAlgorithm(array):
+    maxEndingHere = array[0]
+    maxSoFar = array[0]
+    for i in range(1, len(array)):
+        num = array[i]
+        maxEndingHere = max(num, maxEndingHere + num)
+        maxSoFar = max(maxSoFar, maxEndingHere)
+    return maxSoFar
